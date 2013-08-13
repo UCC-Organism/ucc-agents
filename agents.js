f13fab8935977e0532c106f0e730216b630c5d97
vorg <marcin.ignac@gmail.com>
2013-08-13 16:39:01 +0100

    Not working pyramid rendering

--- agents.js
+++ agents.js
@@ -4,8 +4,11 @@ var Platform = pex.sys.Platform;
 var MathUtils = pex.utils.MathUtils;
 var PerspectiveCamera = pex.scene.PerspectiveCamera;
 var Arcball = pex.scene.Arcball;
+var Cube = pex.geom.gen.Cube;
+var hem = pex.geom.hem;
+var Color = pex.color.Color;
 
-pex.require(['utils/FuncUtils', 'utils/GLX', 'sim/Agent'], function(FuncUtils, GLX, Agent) {
+pex.require(['utils/FuncUtils', 'utils/GLX', 'sim/Agent', 'geom/Pyramid'], function(FuncUtils, GLX, Agent, Pyramid) {
   pex.sys.Window.create({
     settings: {
       width: 1280,
@@ -31,7 +34,11 @@ pex.require(['utils/FuncUtils', 'utils/GLX', 'sim/Agent'], function(FuncUtils, G
       this.arcball = new pex.scene.Arcball(this, this.camera, 5);
       this.framerate(30);
 
-      this.agentMesh = new pex.gl.Mesh(new pex.geom.gen.Cube(0.2), new pex.materials.Test());
+      var pyramid = hem().fromGeometry(new Pyramid()).toFlatGeometry();
+      pyramid = new Pyramid();
+      pyramid2 = new Pyramid();
+      //this.agentMesh = new pex.gl.Mesh(pyramid, new pex.materials.SolidColor(), {useEdges:true});
+      this.agentMesh2 = new pex.gl.Mesh(pyramid2, new pex.materials.Diffuse({wrap:1, ambientColor:Color.Red}));
 
       this.glx = new GLX();
     },
@@ -39,8 +46,11 @@ pex.require(['utils/FuncUtils', 'utils/GLX', 'sim/Agent'], function(FuncUtils, G
       this.agents.forEach(function(agent) {
         agent.update();
       })
-      this.glx.clearColorAndDepth().enableDepthWriteAndRead()
-      this.agentMesh.drawInstances(this.camera, this.agents);
+      this.glx.clearColorAndDepth().enableDepthWriteAndRead().cullFace(false)
+      //this.agentMesh.drawInstances(this.camera, this.agents);
+
+      this.agentMesh2.draw(this.camera);
+      //this.agentMesh.draw(this.camera);
     }
   });
 })
