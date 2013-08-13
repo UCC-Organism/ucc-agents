99e5810dafd1ffaeb6ee2d089f6ae3effe51dc54
vorg <marcin.ignac@gmail.com>
2013-08-13 20:34:04 +0100

    Added arriving behaviour

--- sim/Agent.js
+++ sim/Agent.js
@@ -1,4 +1,4 @@
-define(['pex/geom/Vec3'], function(Vec3) {
+define(['pex/geom/Vec3', 'pex/utils/MathUtils'], function(Vec3, MathUtils) {
 
   if (typeof(Vec3.prototype.limit) === 'undefined') {
     Vec3.prototype.limit = function(maxLength) {
@@ -18,6 +18,7 @@ define(['pex/geom/Vec3'], function(Vec3) {
     this.target = new Vec3(0, 0, 0);
     this.maxSpeed = 0.15;
     this.maxForce = 0.15;
+    this.targetRadius = 2.0;
   }
 
   Agent.prototype.update = function() {
@@ -33,7 +34,15 @@ define(['pex/geom/Vec3'], function(Vec3) {
   }
 
   Agent.prototype.seek = function(target) {
-    this.desired.asSub(target, this.position).normalize().scale(this.maxSpeed);
+    this.desired.asSub(target, this.position);
+    var d = this.desired.length();
+    this.desired.normalize();
+    if (d < this.targetRadius) {
+      this.desired.scale(MathUtils.map(d, 0, this.targetRadius, 0, this.maxSpeed));
+    }
+    else {
+      this.desired.scale(this.maxSpeed);
+    }
     this.steer.asSub(this.desired, this.velocity);
     this.steer.limit(this.maxForce);
     this.applyForce(this.steer);
