8025c305907dab2ef72c21a441f0da4f5b307a9c
vorg <marcin.ignac@gmail.com>
2013-08-13 16:00:22 +0100

    Added basic Agent class

---
+++ sim/Agent.js
@@ -0,0 +1,42 @@
+define(['pex/geom/Vec3'], function(Vec3) {
+
+  if (typeof(Vec3.prototype.limit) === 'undefined') {
+    Vec3.prototype.limit = function(maxLength) {
+      var length = this.length();
+      if (length > maxLength) {
+        this.scale(1/length).scale(maxLength);
+      }
+    }
+  }
+
+  function Agent() {
+    this.position = new Vec3(0, 0, 0);
+    this.velocity = new Vec3(0, 0, 0);
+    this.acceleration = new Vec3(0, 0, 0);
+    this.steer = new Vec3(0, 0, 0);
+    this.desired = new Vec3(0, 0, 0);
+    this.target = new Vec3(0, 0, 0);
+    this.maxSpeed = 1;
+    this.maxForce = 1;
+  }
+
+  Agent.prototype.update = function() {
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.position.add(this.velocity);
+    this.acceleration.scale(0);
+  }
+
+  Agent.prototype.applyForce = function(force) {
+    this.acceleration.add(force);
+  }
+
+  Agent.prototype.seek = function(target) {
+    this.desired.asSub(target, this.position).normalize().scale(this.maxSpeed);
+    this.steer.asSub(this.desired, this.velocity);
+    this.steer.limit(maxForce);
+    this.applyForce(this.steer);
+  }
+
+  return Agent;
+})
